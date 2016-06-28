4ed2e6dbdc2846c1e6395b85b697bb850f9f3874
Oleg Mikhalev <D:\ ()\Effecttools\Effecttools>
2016-06-29 02:14:16 +0300

    chan commit

--- source/js/app.js
+++ source/js/app.js
@@ -11,3 +11,25 @@ var elem = flip[0];
 elem.addEventListener('click', function(e) {
     elem.classList.toggle('flipping');
 });
+
+
+
+a1.click(function(){
+
+
+var image=['Work_1.png', 'Work_2.png', 'Work_3.png']
+var i=$('.slider__center img').data('data-number');
+function forvard(i){
+	i++;
+	var elem='<img src=assets/img/"'+images[i]+'">';
+}
+
+if i===0{
+	i=images.length-1;
+};
+
+if i===2{
+	i=1;
+};
+
+});

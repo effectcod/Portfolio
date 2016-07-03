689ad9ea7927ae5f4f215be4e8466c9f03c201b6
Oleg Mikhalev <D:\ ()\Effecttools\Effecttools>
2016-07-04 02:15:21 +0300

    ch

--- source/js/app.js
+++ source/js/app.js
@@ -6,30 +6,135 @@
 //  }, 1000);
 //})();
 
-var flip = document.getElementsByClassName('flip');
-var elem = flip[0];
-elem.addEventListener('click', function(e) {
-    elem.classList.toggle('flipping');
+
+//////////////////////////////////////////////////////////////////////
+//Preloader-1
+
+$(document).ready(function () {
+	
+	$(function () {
+		var imgs = [];
+
+		$.each($('*'), function(){
+			var $this = $(this),
+			background = $this.css('background-image'),
+			img = $this.is('img');
+			
+
+		if (background != 'none') {
+			var path = background.replace('url("', '').replace('")', '');
+			imgs.push(path);
+		}
+
+		if (img) {
+			var path = $this.attr('src');
+			if (path) {
+				imgs.push(path);
+			}
+		}
+		});
+
+
+		var percents = 1;
+
+		for (var i = 0; i < imgs.length; i++) {
+			var image = $('<img>', {
+				attr: {
+					src : imgs[i]
+				}
+			});
+
+			image.load(function () {
+				setPercents(imgs.length, percents)
+				percents++;
+			});
+		}
+
+		function setPercents(total, current) {
+			var percent = current/total*100;
+
+			if (percent >= 70) {
+				$('.wrap__index').css('display', 'block');
+			}
+
+			$('.loader-bar').css({
+				'width' : percent + '%'
+			}).text(percent + '%');
+		}
+
+	});
+});
+
+//////////////////////////////////////////////////////////////////////
+//Preloader-2
+
+$(window).load(function() { 
+	$('#status').delay(3000).fadeOut(); 
+	$('#preloader').delay(3000).fadeOut('slow'); 
+});
+
+//////////////////////////////////////////////////////////////////////
+//Slaider
+
+
+$(window).load(function() { 
+
+		var counter = 1;
+		$('.slider__controls-top').on('click', function(e){
+			e.preventDefault();
+
+			var $this = $(this),
+					container = $this.closest('.wrap__work'),
+					items = container.find('.slider__item'),
+					activeItem = container.find('.slider__item.active');
+					
+
+			if (counter >= items.length) {counter =0;}
+			console.log(counter);
+
+			var reqItem = items.eq(counter);
+			
+			activeItem.animate({
+				'top' : '100%'
+			}, 300);
+			reqItem.animate({
+				'top' : '0%'
+			}, 300, function () {
+				activeItem.removeClass('active').css('top', '-100%');
+				$(this).addClass('active');
+
+			});
+			counter++;
+	});
+
+
+		
+		$('.slider__controls-bottom').on('click', function(e){
+			e.preventDefault();
+
+			var $this = $(this),
+					container = $this.closest('.wrap__work'),
+					items = container.find('.slider__item'),
+					activeItem = container.find('.slider__item.active');
+					
+
+			if (counter <=-items.length) {counter =0;}
+			console.log(counter);
+
+			var reqItem = items.eq(counter);
+			
+			activeItem.animate({
+				'top' : '100%'
+			}, 300);
+			reqItem.animate({
+				'top' : '0%'
+			}, 300, function () {
+				activeItem.removeClass('active').css('top', '-100%');
+				$(this).addClass('active');
+
+			});
+			counter--;
+	});
 });
 
 
-
-a1.click(function(){
-
-
-var image=['Work_1.png', 'Work_2.png', 'Work_3.png']
-var i=$('.slider__center img').data('data-number');
-function forvard(i){
-	i++;
-	var elem='<img src=assets/img/"'+images[i]+'">';
-}
-
-if i===0{
-	i=images.length-1;
-};
-
-if i===2{
-	i=1;
-};
-
-});
